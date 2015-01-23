8ec5a5f0137a5dfb0af6645de4b6fa914904bdf0
nosarv <nosarv@yahoo.com>
2015-01-23 06:09:58 -0500

    fry

---
+++ ConsoleApp156fr/src/ConsoleApp156fr/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp156fr
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
